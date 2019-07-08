00a71a02b7cba8c05c5e74f4650005e46e62b37e
justficks <firsoff091@gmail.com>
2019-07-08 11:26:48 +0700

    Таблица с основными данными

---
+++ modelofsawingworkshop/src/components/firtsDT.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-data-table :headers="headers" :items="desserts" :loading="loading" class="elevation-1">
+      <template v-slot:items="props">
+        <td class="text-xs-right">{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.avgStoping }}</td>
+        <td class="text-xs-right">{{ props.item.avgReserve }}</td>
+        <td class="text-xs-right">{{ props.item.avgWorking }}</td>
+        <td class="text-xs-right">{{ props.item.avgFixing }}</td>
+        <td class="text-xs-right">{{ props.item.avgLoseMoneyNotWorking }}</td>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    headers: [
+      { text: "Недель прошло", value: "name", align: "center" },
+      { text: "Avg простаивающих", value: "avgStoping", align: "center" },
+      { text: "Avg в резерве", value: "avgReserve", align: "center" },
+      { text: "Avg в работе", value: "avgWorking", align: "center" },
+      { text: "Avg в ремонте", value: "avgFixing", align: "center" },
+      {
+        text: "Потери из-за простоя в неделю",
+        value: "avgLoseMoneyNotWorking",
+        align: "center"
+      }
+    ],
+    desserts: [
+      {
+        name: "",
+        avgStoping: "",
+        avgReserve: "",
+        avgWorking: "",
+        avgFixing: "",
+        avgLoseMoneyNotWorking: ""
+      }
+    ]
+  })
+};
+</script>
